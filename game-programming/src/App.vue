9e9bfe4e85956bda1fbdb2f5c3c3da3f76961fc3
bevis00 <bevis5555555@gmail.com>
2020-06-23 18:34:17 +0800

    background layout and first card model create

--- game-programming/src/App.vue
+++ game-programming/src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-jumbotron
+      id="content1"
+      bg-variant="secondary"
+      text-variant="light"
+      header-level="4"
+    >
+      <b-card
+        title="Works0"
+        img-src="gif/hw0.gif"
+        img-top
+        style="max-width: 20rem;"
+        text-variant="dark"
+      >
+        <b-card-text>
+          私のThree.js初めてのプログラムです、一つモデル動きますのアニメーションです。
+        </b-card-text>
+
+        <b-button href="http://bevis00.github.io/ex/hw0.html" variant="primary"
+          >Link</b-button
+        >
+      </b-card>
+    </b-jumbotron>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
 }
-</script>
 
-<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+}
+
+#content1 {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
 }
 </style>
