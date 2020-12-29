86e144dacd3548c1adb46797dab8c819401bc50e
bevis00 <bevis5555555@gmail.com>
2020-12-29 17:37:34 +0900

    web-3d introduction update

--- web-3d/src/App.vue
+++ web-3d/src/App.vue
@@ -16,14 +16,14 @@
       <div align="center">
         <b-card-group id="cardGroup" columns>
           <b-card
-            title="Works1"
+            title="Works 1"
             img-src="gif/hw1.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              この作品は初めのjQueryの応用、jQuery通してHTMLを操作する、簡単なタンデム操作です。
+              このプログラムは初めてのjQueryを応用することです。jQueryを通じてHTMLを操作することに練習します。画面中真ん中のボタンをクリックして、クリスマスツリーのイルミネーションのように、光が輝いている効果があります。
             </b-card-text>
 
             <b-button
@@ -34,14 +34,14 @@
           </b-card>
 
           <b-card
-            title="Works2"
+            title="Works 2"
             img-src="gif/hw2.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              初めてThree.jsとjQuery組み合わせる、観る可能なホームモデルを作成する、そして、画面にいくつかの固定画角を提供する、視野角の文字をクリックして、視野角を切り替えます。
+              初めてのThree.jsとjQueryを組み合わせるプログラムです。参観できるハウスのモデルを作ります。そして、画面にいくつかの固定画角をセットする。黄色の文字をクリックして、視野角を切り替えます。
             </b-card-text>
 
             <b-button
@@ -52,14 +52,14 @@
           </b-card>
 
           <b-card
-            title="Works3"
+            title="Works 3"
             img-src="gif/hw3.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              会場モデルを作成する、光源の影響を受けるの3Dモデルを使用する、2つの異なる光源を作成する、異なる光源を切り替えることにより、光源通して3Dモデルのプレゼンテーションを観察できます。
+              絵画展のモデルをシミュレーションするプログラムです。光源の影響を受けられる3Dモデルを使います。そして、二つ異なる光源を作って、異なる光源を切り替えることによって、光源の変化することを通じて3Dモデルのプレゼンテーションを観察できます。
             </b-card-text>
 
             <b-button
@@ -70,14 +70,14 @@
           </b-card>
 
           <b-card
-            title="Works4"
+            title="Works 4"
             img-src="gif/hw4.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              これもThree.jsとjQuery組み合わせの応用、右側画面のメニューから選択して、選択した結果は、左側画面をクリックして表示できます。
+              このプログラムはWorks2と同じもThree.jsとjQueryを組み合わせる応用します。右側画面のメニューから選択して、選択した結果は左側画面をクリックして表示できます。
             </b-card-text>
 
             <b-button
@@ -88,26 +88,26 @@
           </b-card>
 
           <b-card
-            title="Works5"
+            title="Works 5"
             img-src="gif/hw5.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              ログインシステムのログテスト、実際のアプリケーションは次の作品で組み合わせる、事前作業の記録です。
+              ログインシステムのログテストです。実際の応用することは次の作品で組み合わせます。これは事前作業の記録です。
             </b-card-text>
           </b-card>
 
           <b-card
-            title="Works6"
+            title="Works 6"
             img-src="gif/hw6.gif"
             img-top
             style="max-width: 25rem; margin-bottom:2rem;"
             text-variant="dark"
           >
             <b-card-text>
-              ウェブページを使用したユーザーの自宅操作のシミュレーション、リンク内は操作の画面記録と操作の説明がある、基本的な原理は、C言語を使用してユーザーデータを記録する、バックエンドでログを生成する、ユーザーはフロントページのログデータを選択して、以前のレコードステータスを復元できます。
+              これはユーザーがウェブサイトで自宅操作することのシミュレーションです。リンク内は操作の画面記録と操作することの説明があります。基本的な原理はC言語を使って、ユーザーデータを記録します。そして、バックエンドにログを生成します。ユーザーはフロントエンドページのログデータを選択して、以前のレコードステータスを復元することができます。
             </b-card-text>
 
             <b-button
